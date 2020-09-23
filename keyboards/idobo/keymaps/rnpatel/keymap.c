9426a2cad16217140c53015495c5db75d61f81e7
Rajiv Patel <rpatel@voalte.com>
2020-09-23 12:07:34 -0400

    made the bottom row shift a one-shot modifier

--- keyboards/idobo/keymaps/rnpatel/keymap.c
+++ keyboards/idobo/keymaps/rnpatel/keymap.c
@@ -58,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
      * │ LShft │   Z   │   X   │   C   │   V   │   B   │  ([{  │       │  }])  │   N   │   M   │   ,   │   .   │   /   │   -   │
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
-     * │T(FUNC)│       │       │ LShft |CTL/SPC│O(LCMD)│O(LOPT)│  ADJ  │ ROPT  │ RCMD  │  SPC  │   \   │       │       │T(FUNC)│
+     * │T(FUNC)│       │       │ O(LSH)|CTL/SPC│O(LCMD)│O(LOPT)│  ADJ  │ ROPT  │ RCMD  │  SPC  │   \   │       │       │T(FUNC)│
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
-    [QWERTY] = LAYOUT_ortho_5x15( KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,            KC_5,           SCMD(KC_LALT),  KC_UP,      ALL_T(KC_APP),  KC_6,     KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC, \
-                                  KC_GRV,   KC_Q,     KC_W,     KC_E,     KC_R,            KC_T,           KC_LEFT,        KC_DOWN,    KC_RGHT,        KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_EQL,  \
-                                  KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,            KC_G,           KC_TAB,         XXXXXXX,    KC_ENT,         KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-                                  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,            KC_B,           TD(TD_LBRKT),   XXXXXXX,    TD(TD_RBRKT),   KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS, \
-                                  TT(FUNC), XXXXXXX,  XXXXXXX,  KC_LSFT,  LCTL_T(KC_SPC),  OSM(MOD_LGUI),  OSM(MOD_LALT),  MO(ADJUST), KC_RALT,        KC_RGUI,  KC_SPC,  KC_BSLS,  XXXXXXX,  XXXXXXX,  TT(FUNC) ),
+    [QWERTY] = LAYOUT_ortho_5x15( KC_ESC,   KC_1,     KC_2,     KC_3,           KC_4,            KC_5,           SCMD(KC_LALT),  KC_UP,      ALL_T(KC_APP),  KC_6,     KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC, \
+                                  KC_GRV,   KC_Q,     KC_W,     KC_E,           KC_R,            KC_T,           KC_LEFT,        KC_DOWN,    KC_RGHT,        KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_EQL,  \
+                                  KC_LCTL,  KC_A,     KC_S,     KC_D,           KC_F,            KC_G,           KC_TAB,         XXXXXXX,    KC_ENT,         KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+                                  KC_LSFT,  KC_Z,     KC_X,     KC_C,           KC_V,            KC_B,           TD(TD_LBRKT),   XXXXXXX,    TD(TD_RBRKT),   KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS, \
+                                  TT(FUNC), XXXXXXX,  XXXXXXX,  OSM(MOD_LSFT),  LCTL_T(KC_SPC),  OSM(MOD_LGUI),  OSM(MOD_LALT),  MO(ADJUST), KC_RALT,        KC_RGUI,  KC_SPC,  KC_BSLS,  XXXXXXX,  XXXXXXX,  TT(FUNC) ),
 
     /* FUNC
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
