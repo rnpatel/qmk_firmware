491851b6fe21e64f381bd9a556ae31194eff1511
Rajiv Patel <rpatel@voalte.com>
2021-02-26 16:59:44 -0500

    rnp
    - add GAME layer

--- keyboards/idobo/keymaps/rnpatel/keymap.c
+++ keyboards/idobo/keymaps/rnpatel/keymap.c
@@ -19,6 +19,7 @@
 enum layers
 {
     QWERTY,
+    GAME,
     FUNC,
     ADJUST
 };
@@ -56,8 +57,8 @@ const rgblight_segment_t PROGMEM func_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 
 // Light the middle green when ADJUST is active
 const rgblight_segment_t PROGMEM adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {2, 4, HSV_ORANGE},
-    {10, 4, HSV_ORANGE}
+    {3, 2, HSV_GREEN},
+    {11, 2, HSV_GREEN}
 );
 
 
@@ -104,6 +105,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
                                   KC_LSFT,   KC_Z,     KC_X,     KC_C,           KC_V,            KC_B,           TD(TD_LBRKT),   XXXXXXX,     TD(TD_RBRKT),   KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS, \
                                   TT(FUNC),  XXXXXXX,  XXXXXXX,  OSM(MOD_LSFT),  LCTL_T(KC_SPC),  OSM(MOD_LGUI),  OSM(MOD_LALT),  MO(ADJUST),  KC_RALT,        KC_RGUI,  KC_SPC,  KC_BSLS,  XXXXXXX,  XXXXXXX,  TT(FUNC) ),
 
+    /* GAME
+     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
+     * │ Esc   │   1   │   2   │   3   │   4   │   5   │  MEH  │  Up   │ HYPER │   6   │   7   │   8   │   9   │   0   │ Bksp  │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │   `   │   Q   │   W   │   E   │   R   │   T   │ Left  │ Down  │ Right │   Y   │   U   │   I   │   O   │   P   │   =   │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Ctrl  │   A   │   S   │   D   │   F   │   G   │  TAB  │       │ ENTER │   H   │   J   │   K   │   L   │   ;   │   '   │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ LShft │   Z   │   X   │   C   │   V   │   B   │   [   │       │   ]   │   N   │   M   │   ,   │   .   │   /   │   -   │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │M(FUNC)│       │       │ LShft | Space │ LCMD  │ LOPT  |       │ ROPT  │ RCMD  │  SPC  │   \   │       │       │QWERTY │
+     * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
+     */
+    [GAME] = LAYOUT_ortho_5x15( KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,    KC_5,     SCMD(KC_LALT),  KC_UP,    ALL_T(KC_APP),  KC_6,     KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC,   \
+                                KC_GRV,    KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,     KC_LEFT,        KC_DOWN,  KC_RGHT,        KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_EQL,    \
+                                KC_LCTL,   KC_A,     KC_S,     KC_D,     KC_F,    KC_G,     KC_TAB,         XXXXXXX,  KC_ENT,         KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,   \
+                                KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,     KC_LBRC,        XXXXXXX,  KC_RBRC,        KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS,   \
+                                MO(FUNC),  XXXXXXX,  XXXXXXX,  KC_LSFT,  KC_SPC,  KC_LGUI,  KC_LALT,        XXXXXXX,  KC_RALT,        KC_RGUI,  KC_SPC,  KC_BSLS,  XXXXXXX,  XXXXXXX,  DF(QWERTY) ),
+
     /* FUNC
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
      * │  Esc  │  F1   │  F2   │  F3   │  F4   │  F5   │  MEH  │       │ HYPER │  F6   │  F7   │  F8   │  F9   │  F10  │ Bksp  │
@@ -133,14 +153,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
      * │RGB_TOG│       │       │       │       │       │       │       │       │       │       │       │       │       │       │
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
-     * │       │       │       │       │       │       │       │  ADJ  │       │       │       │       │       │       │       │
+     * │       │       │       │       │       │       │       │  ADJ  │       │       │       │       │       │       │  GAME │
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [ADJUST] = LAYOUT_ortho_5x15( XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
                                   RGB_MOD,   RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_SPI,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
                                   RGB_RMOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_SPD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
                                   RGB_TOG,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
-                                  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX  ),
+                                  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  DF(GAME) ),
 };
 
 void tapdance_lbrkt(qk_tap_dance_state_t *state, void *user_data)
